08beffb916eb3e454a70a95ad6e74bfafacb2f73
Anastasia Okuonghae <aokuonghae@gmail.com>
2020-10-01 16:27:37 -0500

    removed extra variable. count only letters

--- CountingCharacters/Program.cs
+++ CountingCharacters/Program.cs
@@ -13,13 +13,12 @@ namespace CountingCharacters
             if (File.Exists(textFile))
             {
                string inputString = File.ReadAllText(textFile);
-               string lowerCaseInput = inputString.ToLower();
 
                 //I am using linq and will require an import which is at the top of my file. 
                 // Really nifty stuff:  https://www.tutorialsteacher.com/linq/linq-query-syntax 
 
                 //METHOD 1
-                var charGroups = (from characters in lowerCaseInput
+                var charGroups = (from characters in inputString.Trim().ToLower().Where(char.IsLetter)
                                   group characters by characters into charGroup
                                   select String.Format("{0}:{1} \n", charGroup.Key, charGroup.Count()));
                 //String.format is a .NET method. https://docs.microsoft.com/en-us/dotnet/api/system.string.format?view=netcore-3.1
